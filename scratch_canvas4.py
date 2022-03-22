7c0897ca2b50b4ef61c304e77d4616b7bf6c32b5
PalPatel27 <101315887+PalPatel27@users.noreply.github.com>
2022-03-22 16:38:06 +0530

    Add files via upload

---
+++ scratch_canvas4.py
@@ -0,0 +1,8 @@
+marks = int(input('enter your marks'))
+if marks >= 80:
+    print ("excellent work")
+elif marks >= 60:
+    print ("good work")
+elif marks < 60:
+    print ("you can do better")
+    
